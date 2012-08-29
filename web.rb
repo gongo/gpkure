4e4026475fee9dc413cba8b828b349bcaa4c8eeb
Wataru MIYAGUNI <gonngo@gmail.com>
2012-08-29 23:54:25 +0900

    start

---
+++ web.rb
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+require 'sinatra'
+require 'haml'
+
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+get '/' do
+  @url = request.host + (request.port == 80 ? '' : ":#{request.port.to_s}")
+  @count = REDIS.scard('serial')
+  haml :index, :format => :html5
+end
+
+post '/' do
+  serial = params[:serial]
+  if /^(\d{16})$/ =~ serial
+    REDIS.sadd('serial', serial)
+    "ok"
+  end
+end
+
+__END__
+
+@@ index
+!!!
+%html
+  %head
+    %title Georgia Point Kure
+    :css
+      #wrapper {
+          width: 800px;
+          height: 200px;
+          margin: 0 auto;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin: -100px 0 0 -400px;
+      }
+
+      #wrapper > span {
+          font-size: 32px;
+          font-weight: bold;
+      }
+  %body
+    %div#wrapper
+      <span>$ curl -d 'serial=/\d{16}/' #{@url}</span>
